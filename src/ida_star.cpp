4e02c14f5d8a10a49b14bdecc336e1f01c0fda07
rahulsridhar <rahulsridhar92@gmail.com>
2016-11-27 22:17:06 -0800

    Updated with heuristic

--- src/ida_star.cpp
+++ src/ida_star.cpp
@@ -2,18 +2,24 @@
 #include "ida_star.h"
 #include "problem.h"
 #include "state_space.h"
-
-// Placeholder for including heuristic-related files
+#include "heuristic.h"
+//#include "bbsolver.h"
+//#include "assignmentsolver.h"
 
 #include <vector>
 #include <stdlib.h>
 #include <iostream>
+#include <algorithm>
+#include <climits>
+#include <unordered_set>
+
 
 // Temporary heuristic function - to be modified once heuristic classes are finalized
-unsigned int h(State *state, unsigned int f)
+unsigned int h(State &state, unsigned int f)
 {
 	return (100-f)/10;
 }
+
 
 // Initialize cut-off bound of IDA* search to 0 and create goal, initial state objects
 ida_star::ida_star()
@@ -21,23 +27,28 @@ ida_star::ida_star()
 	bound = 0;
 	init_state = new State_Space;
 	goal = new State_Space;
+	//Heuristic heur;
+
 }
 
 // Begin IDA* search from root node
-std::vector<Action> ida_star::ida_begin(State root, Problem *test_problem)
+std::vector<Action> ida_star::ida_begin(State &root, Problem &test_problem, AssignmentSolver &solver)
 {
 	// Reset bound to the heuristic cost to reach the goal from the root
-	bound = h(&root, 0); 
+	Heuristic heur(&root, &solver);
+	bound = heur.evaluate(root);
 	unsigned int result;
 	
 	// Assign values to the initial state of the state space
 	init_state->current = root;
 	init_state->parent_to_curr = static_cast <Action> (-1);
 	init_state->parent = NULL;
-	
+	init_state->visited = false;	
+
 	while(true)
 	{
-		result = search(init_state, 0, test_problem); // IDA* search from the root
+		//init_state->current.print();
+		result = search(init_state, 0, test_problem, heur); // IDA* search from the root
 		if(result == 0) // Reached goal state
 		{
 			State_Space *traverse = new State_Space;
@@ -65,58 +76,72 @@ std::vector<Action> ida_star::ida_begin(State root, Problem *test_problem)
 }
 
 // Generate resultant states from making all valid actions from current state
-std::vector<State_Space*> ida_star::generate_successors(State_Space *state, Problem *test_problem)
+void ida_star::generate_successors(State_Space *state, Problem &test_problem, std::vector<State_Space>& successors)
 {
+	using namespace std;
+
 	// Get valid actions from current state
-	std::vector<Action> actions = test_problem->valid_actions(&state->current);
+	std::vector<Action> actions = test_problem.valid_actions(&state->current);
 	std::vector<State_Space*> gen_successors;
 	
+	std::cout << "In successors" << std::endl;
 	for(std::vector<Action>::iterator it = actions.begin(); it!=actions.end(); ++it)
 	{
 		State_Space *next = new State_Space;	
 		
 		// Get and store the successors of current state
-		next->current = test_problem->result(&state->current, *it);
+		const State &s = test_problem.result(&state->current, *it);
+		next->current = s;
 		next->parent_to_curr = *it;
 		next->parent = state;
-		
 		// Check if state is the goal and store it
-		if(test_problem->goal_test(&next->current))
+		if(test_problem.goal_test(&next->current))
         		goal = next; 
-		gen_successors.push_back(next);
+		successors.push_back(*next);
 				
 	}
-	return gen_successors;	
+	//return gen_successors;	
 }
 
-unsigned int ida_star::search(State_Space *state, unsigned int g, Problem *test_problem)
+unsigned int ida_star::search(State_Space *state, unsigned int g, Problem &test_problem, Heuristic &heur)
 {
 	// Calculate f value of the state
-	unsigned int f = g + h(&state->current, g);
-	
- 	// Return f if f value is > cutoff bound
+	//unsigned int f = g + h(state->current, g); // Naive heuristic
+	unsigned int f = g + heur.evaluate(state->current); // Manhattan heuristic
+	//unsigned int f = g; // UCS
+ 	
+	// Return f if f value is > cutoff bound
 	if(f > bound)
 		return f;
 
 	// Return 0 if we have reached the goal state
-	if(test_problem->goal_test(&state->current) == 1)
+	if(test_problem.goal_test(&state->current) == 1)
 		return 0;
 
-	
-	std::vector<State_Space*> successors;
 	unsigned int min = UINT_MAX, temp_result;
 
 	// Generate successors of current state
-	successors = generate_successors(state, test_problem);
+	State_Space *next = new State_Space;
+	
+	std::vector<Action> actions = test_problem.valid_actions(&state->current);
+	if(actions.empty())
+		return UINT_MAX;	
 
-	for(auto it = successors.begin(); it!=successors.end(); ++it)
-	{
-		// Recursive search - assuming unit cost for each move
-		temp_result = search(*it, g+1, test_problem); 
-		if(temp_result == 0) // Reached goal state
-			return 0;
-		if(temp_result < min) // Update bound
-			min = temp_result;
-	}
+	for(std::vector<Action>::iterator it = actions.begin(); it!=actions.end(); ++it)
+        {
+                // Get and store the successors of current state
+                State s = test_problem.result(&state->current, *it);
+                next->current = s;
+		next->parent_to_curr = *it;
+                next->parent = state;
+                if(test_problem.goal_test(&next->current))
+                        goal = next;
+		temp_result = search(next, (g+100), test_problem, heur);
+	        if(temp_result == 0) // Reached goal state
+                   	return 0;
+	        if(temp_result < min) // Update bound
+        	        min = temp_result;
+        }
+	
 	return min;
 }
