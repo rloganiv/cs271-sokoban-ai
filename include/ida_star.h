f0627011b7a4803ef932aadb12624e0645bdb424
rahulsridhar <rahulsridhar92@gmail.com>
2016-11-27 22:16:52 -0800

    Updated with heuristic

--- include/ida_star.h
+++ include/ida_star.h
@@ -4,9 +4,11 @@
 #include "problem.h"
 #include "state.h"
 #include <vector>
-#include <algorithm>
-#include <climits>
 #include "state_space.h"
+#include "heuristic.h"
+#include "assignmentsolver.h"
+#include "bbsolver.h"
+#include <unordered_set>
 
 class ida_star {
 
@@ -14,18 +16,19 @@ class ida_star {
 		ida_star();
 		
 		// Begin IDA* search from the initial state
-		std::vector<Action> ida_begin(State root, Problem *test_problem);
+		std::vector<Action> ida_begin(State &root, Problem &test_problem, AssignmentSolver &solver);
 
 	private:
 		// Bound for each search iteration
 		unsigned int bound;
 
 		// Recursive search from the successors of each state
-		unsigned int search(State_Space *state, unsigned int g, Problem *test_problem);
+		unsigned int search(State_Space *state, unsigned int g, Problem &test_problem, Heuristic &heur);
 		
 		// Generate successors of current state
-		std::vector<State_Space*> generate_successors(State_Space *state, Problem *test_problem);
-		
+		//std::vector<State_Space> generate_successors(State_Space *state, Problem &test_problem);
+		void generate_successors(State_Space *state, Problem &test_problem, std::vector<State_Space>& successors);
+
 		// Store the Initial state and Goal state
 		State_Space *init_state, *goal;
 		
