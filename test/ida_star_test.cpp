b7d578b2bde31c9a1e3943813e1e1302a2779d06
Robert Lockwood Logan IV <rloganiv@gmail.com>
2016-11-27 19:57:05 -0800

    Updated ida_star_test to work with updated problem class

--- test/ida_star_test.cpp
+++ test/ida_star_test.cpp
@@ -6,37 +6,38 @@
 
 int main() {
     using namespace std;
-    Problem test_problem;
+    Problem test_problem("courseTestFile");
 
     cout << "Initializing problem from file" << endl;
-    State init_state = test_problem.init_from_file("courseTestFile");
+    State *init_state;
+    init_state = test_problem.get_init_state();
     cout << "Printing initial state" << endl;
-    init_state.print();
+    init_state->print();
 
     cout << "Checking if initial state is a goal" << endl;
-    cout << test_problem.goal_test(&init_state) << endl;
+    cout << test_problem.goal_test(init_state) << endl;
 
     cout << "Calling IDA*" << endl;
     std::vector<Action> path_to_goal;
-    
+
     ida_star ida;
-    path_to_goal = ida.ida_begin(init_state, &test_problem);
+    path_to_goal = ida.ida_begin(*init_state, &test_problem);
     cout << "Returned from IDA* " << endl;
-    
+
     if(path_to_goal.empty())
     {
-	cout << " No solution found " << endl;  
+	cout << " No solution found " << endl;
 	return -1;
     }
     else
     {
 	cout << "Number of moves to the goal = " <<path_to_goal.size()<< endl;
     	cout << "Path to goal = ";
-    	
-	// Print the path to the goal state 
+
+	// Print the path to the goal state
     	for(auto it = path_to_goal.begin(); it!= path_to_goal.end(); ++it)
 		cout <<  *it << " ";
- 	cout << endl; 
+ 	cout << endl;
     }
     return 0;
 }
