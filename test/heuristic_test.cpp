d76d832ff30b0e3bcdddc1f8d347a71da2dc7e45
Robert Lockwood Logan IV <rloganiv@gmail.com>
2016-11-27 20:02:44 -0800

    Updated heuristic_test to work with updated problem class

--- test/heuristic_test.cpp
+++ test/heuristic_test.cpp
@@ -13,40 +13,40 @@ using namespace std;
 
 
 void runTest(string testfile){
-	Problem test_problem;
+	Problem test_problem(testfile);
 
     cout << "Initializing problem from file" << endl;
-    State init_state = test_problem.init_from_file(testfile);
+    State* init_state = test_problem.get_init_state();
     cout << "Printing initial state" << endl;
-    init_state.print();
+    init_state->print();
 
     BBSolver solver;
     cout << "Creating heuristic" << endl;
     cout << "Scale factor of a single step: " << K::STEP_SCALE << endl;
-    Heuristic h(&init_state, &solver);
+    Heuristic h(init_state, &solver);
     cout << "Unmovability table" << endl;
     h.print_unmovability_table();
-    cout << "Goal score: " << h.goal_score(init_state, h.make_coord(1,3)) << endl;
-    cout << "Next to block bonus (currently unused): " << h.next_to_block_bonus(init_state) << endl;
-    cout << "Manhattan Dist score: " << h.manhattan_dist_score(init_state) << endl;
-    cout << "Player pos bonus: " << h.player_pos_bonus(init_state) << endl;
-    cout << "Eval score: " << h.evaluate(init_state) << endl;
-    
+    cout << "Goal score: " << h.goal_score(*init_state, h.make_coord(1,3)) << endl;
+    cout << "Next to block bonus (currently unused): " << h.next_to_block_bonus(*init_state) << endl;
+    cout << "Manhattan Dist score: " << h.manhattan_dist_score(*init_state) << endl;
+    cout << "Player pos bonus: " << h.player_pos_bonus(*init_state) << endl;
+    cout << "Eval score: " << h.evaluate(*init_state) << endl;
+
     cout << "Move up" << endl;
-    State s1 = test_problem.result(&init_state, UP);
+    State s1 = test_problem.result(init_state, UP);
     s1.print();
-    
+
     cout << "Goal score: " << h.goal_score(s1, h.make_coord(1,3)) << endl;
     cout << "Next to block bonus (currently unused): " << h.next_to_block_bonus(s1) << endl;
     cout << "Manhattan Dist score: " << h.manhattan_dist_score(s1) << endl;
     cout << "Player pos bonus: " << h.player_pos_bonus(s1) << endl;
     cout << "Eval score: " << h.evaluate(s1) << endl;
-    
+
     cout << "Move right" << endl;
     State s2 = test_problem.result(&s1, RIGHT);
     s2.print();
-    
-    
+
+
     cout << "Goal score: " << h.goal_score(s2, h.make_coord(1,3)) << endl;
     cout << "Next to block bonus (currently unused): " << h.next_to_block_bonus(s2) << endl;
     cout << "Manhattan Dist score: " << h.manhattan_dist_score(s2) << endl;
@@ -58,7 +58,7 @@ int main() {
 
     string test0 = "courseTestFile";
     string test1 = "std_suite/screen.1";
-    
+
     runTest(test0);
     runTest(test1);
 
