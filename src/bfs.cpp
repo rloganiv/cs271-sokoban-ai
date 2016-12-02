5cd2f7a9bd209c317c98daf7a961e1e864e41f8e
rahulsridhar <rahulsridhar92@gmail.com>
2016-12-01 16:54:30 -0800

    Updated to print goal path

--- src/bfs.cpp
+++ src/bfs.cpp
@@ -1,7 +1,6 @@
 #include "state.h"
 #include "problem.h"
 #include "state_space.h"
-
 #include <vector>
 #include <stdlib.h>
 #include <iostream>
@@ -10,17 +9,9 @@
 #include <queue>
 #include <unordered_set>
 #include "bfs.h"
+#include <list>
 
-int num_goals = 1;
-
-// Temporary heuristic function - to be modified once heuristic classes are finalized
-unsigned int h(State &state, unsigned int f)
-{
-	return (100-f)/10;
-}
-
-
-// Initialize cut-off bound of IDA* search to 0 and create goal, initial state objects
+// Create initial state and goal state objects
 bfs::bfs()
 {
 	init_state = new State_Space;
@@ -29,65 +20,60 @@ bfs::bfs()
 
 }
 
-// Begin IDA* search from root node
+// Begin BFS search from root node
 std::vector<Action> bfs::bfs_begin(State &root, Problem &test_problem)
 {
 	// Reset bound to the heuristic cost to reach the goal from the root
 	unsigned int result;
-
 	// Assign values to the initial state of the state space
 	init_state->current = root;
 	init_state->parent_to_curr = static_cast <Action> (-1);
 	init_state->parent = NULL;
 	init_state->visited = false;
 	frontier.push(*init_state);
-	//visited.insert(&init_state->current);
 	visited.insert(init_state->current);
-	//State_Space visiting = new State_Space;
+	std::list<State_Space> visiting;
 
 	//std::cout << "Check = " << heur.manhattan_dist_score(init_state->current) << std::endl;
 	while(!frontier.empty())
 	{
-		//State_Space *visiting = new State_Space;
-		auto visiting = frontier.front();
+		auto temp = frontier.front();
+		visiting.push_back(frontier.front());
 		frontier.pop();
 
 		std::cout << "In bfs_begin" << std::endl;
 		//visiting.current.print();
 
-		std::vector<Action> actions = test_problem.valid_actions(&visiting.current);
-
+		//std::vector<Action> actions = test_problem.valid_actions(&visiting.current);
+		std::vector<Action> actions = test_problem.valid_actions(&temp.current);
+	
 		for(std::vector<Action>::iterator it = actions.begin(); it!=actions.end(); ++it)
 	        {
 			State_Space* next = new State_Space;
                 	// Get and store the successors of current state
-               		const State &s = test_problem.result(&visiting.current, *it);
-	                next->current = s;
+	                const State &s = test_problem.result(&temp.current, *it);
+			next->current = s;
         	        next->parent_to_curr = *it;
-                	next->parent = &visiting;
-
-	                std::cout << "Visiting - child" << *it << std::endl;
+			next->parent = &(visiting.back());
+	                std::cout << "Visiting - child" << *it << std::endl<<std::endl;
         	        (next->current).print();
-                	// Check if state is the goal and store it
+                	
+			// Check if state is the goal and store it
 	                if(test_problem.goal_test(&next->current))
         	        {
 				std::cout<<"Reached the goal "<<std::endl;
 		        	goal = next;
 				State_Space *traverse = new State_Space;
         	                traverse = goal;
-			/*	//(traverse->parent->current).print();
-				//(traverse->parent->parent->current).print();
-				//std::cout<<traverse->parent_to_curr<<std::endl;
-                	        // Get the set of moves from goal to initial state
+                	     
+				// Get the set of moves from goal to initial state
                         	while(traverse->parent != NULL)
-	                        //while(!test_problem.states_equal(&traverse->current, &init_state->current) )
 				{
-					//std::cout<<traverse->parent_to_curr<<std::endl;
         	                        path_to_goal.push_back(traverse->parent_to_curr);
                         	        traverse = traverse->parent;
                                 }
-			*/
-                        // Reverse the list to get the set of moves from initial state to goal
+			
+	                        // Reverse the list to get the set of moves from initial state to goal
                         	std::reverse(path_to_goal.begin(), path_to_goal.end());
 	                        return path_to_goal;
 		    	}
